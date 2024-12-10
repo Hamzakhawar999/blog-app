19b749dd9b95ede59713a1d113aa0d660367b0f2
Hamzakhawar999 <hkhawar242@gmail.com>
2024-12-10 17:36:55 +0500

    hey

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -2,12 +2,31 @@ import dbConnect from "@/app/Config/db";
 import user from "@/app/Models/user";
 import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
+import Cors from "cors";
+
+// Initialize CORS middleware
+const cors = Cors({
+  methods: ['GET', 'POST', 'OPTIONS'], // Allowed methods
+  origin: '*', // Replace with your front-end domain or use '*' for all origins
+});
+
+const runMiddleware = (req, res, fn) =>
+  new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
 
 export async function POST(req) {
+  // Run the CORS middleware for each request
+  await runMiddleware(req, req, cors);
+
   await dbConnect();
   try {
     const data = await req.json();
-
 
     if (!data.password) {
       return NextResponse.json(
@@ -23,7 +42,6 @@ export async function POST(req) {
 
     const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
- 
     data.password = hashedPassword;
 
     const response = await user.create(data);
